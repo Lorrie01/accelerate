cdb794e314da1e947718a6df640ce62e14890477
Lorrie Pearson <pearsonlorrie@gmail.com>
2017-06-05 17:43:19 -0400

    added twitter to home

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -57,18 +57,35 @@ get_header(); ?>
 </section>
 
 <section class="recent-posts">
- <div class="site-content">
-  <div class="blog-post">
-   <h4>From the Blog</h4>
-    <?php query_posts('posts_per_page=1'); ?>
-     <?php while ( have_posts() ) : the_post(); ?>
-       <h2><?php the_title(); ?></h2>
-       <?php the_excerpt(); ?> 
-       <a class="read-more-link" href="<?php the_permalink(); ?>">Read More <span>&rsaquo;</span></a>
-     <?php endwhile; ?> 
-    <?php wp_reset_query(); ?>
-   </div>
-  </div>
+	<div class="site-content">
+        
+		<div class="blog-post">
+			<h4>From the Blog</h4>
+			<?php query_posts('posts_per_page=1'); ?>
+			<?php while (have_posts()) : the_post(); ?>
+				<h2><?php the_title(); ?></h2>
+       <?php the_excerpt(); ?>
+       <a href="<?php the_permalink(); ?>" class="read-more-link">Read More <span>&rsaquo;</span></a>
+		 <?php endwhile; //end of the loop. ?>
+			<?php wp_reset_query();  // resets the altered query back to the original ?>
+		</div>
+        
+        
+        <?php 
+		    $twitter_link = get_field('twitter_link');
+		    $link_name = get_field('link_name');
+		?>
+        
+		<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+      <div id="secondary" class="widget-area" role="complementary">
+				<h4>Recent Tweet</h4>
+	  <?php dynamic_sidebar( 'sidebar-2' ); ?>
+		<a href="<?php the_permalink(); ?>" class="follow-us-link">Follow Us<span>&rsaquo;</span></a>
+     </div>
+    <?php endif; ?>
+        
+ </div>
 </section>
+
 
 <?php get_footer(); ?>
